6d54b7ee2dff08f38b8c00a49896e041f9e50059
gabr1el94 <biel_cazacaza@hotmail.com>
2017-11-26 20:04:34 -0200

    estrutura pasta

---
+++ ePaperWeb/web/resources/js/script.js
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(document).ready(function() {
+    var $ = jQuery;
+    $(document).ready(function() {
+        $("input[id*='dataNascimento']").mask('99/99/9999');
+    });
+
+});
+
